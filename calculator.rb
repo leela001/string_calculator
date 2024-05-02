578ed2bc50a29774023160736707bc416a8ed97b
Leelapavan <leelapavankumarchitirala25@gmail.com>
2024-05-02 22:52:55 +0530

    Implement addition method for summing comma-separated numbers
    Implemented the addition method according to the provided requirements. Added tests to cover the simplest cases of an empty string and single numbers, as well as cases with multiple comma-separated numbers. This method returns the sum of the numbers parsed from the input string.
     main

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,18 @@
 class Calculator
 
-    
+    def add(str_input)
+        return 0 unless !str_input.empty?
+
+        # default delimiter
+        delimitor = ","
+
+        # split the string input into an array of substrings using the delimiter
+        substring_input = str_input.split(delimitor)
+
+        # convert each substring to an integer and returns the integer array
+        num_array = substring_input.map(&:to_i)
+
+        # calculates the sum of integers
+        num_array.sum
+    end
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -1,7 +1,26 @@
-require 'calculator'
+require 'rspec'
+require_relative 'calculator'
 
 RSpec.describe Calculator do
 
+    describe 'addition' do
 
+        it 'test when the input is empty string' do
+            calc = Calculator.new
+            res = calc.add("")
+            expect(res).to eq(0)
+        end
 
+        it 'test when the input contains only single digit' do
+            calc = Calculator.new
+            res = calc.add("1")
+            expect(res).to eq(1)
+        end
+
+        it 'test when the input contains multiple digits with comma(,) separation' do
+            calc = Calculator.new
+            res = calc.add("1,2,3,4,5")
+            expect(res).to eq(15)
+        end
+    end
 end
